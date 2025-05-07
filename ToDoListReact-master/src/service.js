c336158c8c392553ff99d83f2183a5a4269853ea
nechami <nechami>
2025-05-07 19:40:56 +0300

    tIYAR2

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -5,15 +5,29 @@ axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 
 export default {
+
+  // getTasks: async () => {
+  //   try {
+  //     const result = await axios.get(``);
+  //     return result.data || []; // מחזיר מערך ריק אם result.data הוא undefined
+  //   } catch (err) {
+  //     console.error("Error", err);
+  //     return []; // מחזיר מערך ריק במקרה של שגיאה
+  //   }
+  // },
+
   getTasks: async () => {
     try {
       const result = await axios.get(``);
+      console.log("Data received from API:", result.data); // לוג של הנתונים שהתקבלו
+      console.log("Response status:", result.status); // לוג של הסטטוס של הבקשה
       return result.data || []; // מחזיר מערך ריק אם result.data הוא undefined
     } catch (err) {
-      console.error("Error", err);
+      console.error("Error fetching data from API:", err); // לוג של השגיאה במקרה של בעיה
       return []; // מחזיר מערך ריק במקרה של שגיאה
     }
   },
+  
 
   addTask: async (name) => {
     const item = { name: name };
